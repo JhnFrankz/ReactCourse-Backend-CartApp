7a54d692004d3067ee2a11a7febf6bf5bc597c17
Jhan Frank <jhnfrankz@gmail.com>
2023-06-25 10:46:46 -0500

    fix

--- src/main/java/com/jhnfrankz/backend/cartapp/backendcartapp/controllers/ProductController.java
+++ src/main/java/com/jhnfrankz/backend/cartapp/backendcartapp/controllers/ProductController.java
@@ -3,6 +3,7 @@ package com.jhnfrankz.backend.cartapp.backendcartapp.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import com.jhnfrankz.backend.cartapp.backendcartapp.services.ProductService;
 
 // es rest controller porque va a devolver un json y no una vista
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class ProductController {
     
     @Autowired
